438b8cbbb1636943b6514462fa9778d65b57293e
jacob adjei <jacob@adjei.co.uk>
2026-06-10 14:58:12 +0100

    Created Task model Created Priority and Status enum
    Created and updated project files:
    - application.properties
    - docker-compose.yml

---
+++ to-do-list-project/src/main/java/tdl/demo/enumerator/Priority.java
@@ -0,0 +1,10 @@
+package tdl.demo.enumerator;
+
+public enum Priority {
+
+
+    P0,
+    P1,
+    P2,
+    P3
+}
---
+++ to-do-list-project/src/main/java/tdl/demo/model/Task.java
@@ -0,0 +1,36 @@
+package tdl.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tdl.demo.enumerator.Priority;
+import tdl.demo.enumerator.Status;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    // Could add @NotBlank
+    // Eventually could make it so blank task cards are automatically deleted
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    private LocalDateTime dueDate;
+}
